2acbe39e58ec10799bf25fe1526938d94aae7ffa
otamir <omer@diversion.dev>
2025-05-18 12:32:27 +0300

    hmm

--- gen/ir/media.go
+++ gen/ir/media.go
@@ -23,8 +23,6 @@ const (
 	EncodingOctetStream Encoding = "application/octet-stream"
 	// EncodingTextPlain is Encoding for text.
 	EncodingTextPlain Encoding = "text/plain"
-	// EncodingNDJSON is Encoding for newline-delimited JSON.
-	EncodingNDJSON Encoding = "application/x-ndjson"
 )
 
 func (t Encoding) String() string { return string(t) }
@@ -38,8 +36,6 @@ func (t Encoding) MultipartForm() bool { return t == EncodingMultipart }
 func (t Encoding) OctetStream() bool { return t == EncodingOctetStream }
 
 func (t Encoding) TextPlain() bool { return t == EncodingTextPlain }
-
-func (t Encoding) NDJSON() bool { return t == EncodingNDJSON }
 
 type Media struct {
 	// Encoding is the parsed content type used for encoding, but not for header value.
